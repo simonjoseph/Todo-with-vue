7df94490e2c477074145294d30e8838b73954578
simonjoseph <simaojose1978@gmail.com>
2024-06-04 15:05:20 +0100

    finalizando o dark mode

--- main.js
+++ main.js
@@ -1,26 +1,58 @@
 const app = Vue.createApp({
-    data(){
-        return{
+    data() {
+        return {
             textTodo: '',
-            todos:[]
+            activeFilter: 'all',
+            todos: [],
+            currentDate: ''
         }
     },
-    methods:{
-        displaySidebar(){
+    methods: {
+        displaySidebar() {
 
             document.querySelector('.sidebar').classList.toggle('display-sidebar');
         },
-        addTodo(){
-            this.todos.push({name: this.textTodo})
+        addTodo() {
+            const now = new Date();
+            const day = String(now.getDate()).padStart(2, '0');
+            const month = String(now.getMonth() + 1).padStart(2, '0');
+            const year = now.getFullYear();
+
+            this.currentDate = `${year}-${month}-${day}`;
+
+            if (this.textTodo === '') return;
+            this.todos.push({
+                name: this.textTodo,
+                date: this.currentDate,
+                status: 'uncompleted'
+            })
             this.textTodo = '';
         },
-        doneTodo(){
-            console.log('done');
+        doneTodo(index) {
+            this.todos[index].status = 'completed';
         },
-        deleteTodo(){
-            console.log('delete');
+        deleteTodo(index) {
+            this.todos.splice(index, 1);
+        },
+        filterTodos(value) {
+            this.activeFilter = value;
+        },
+        filteredTodos() {
+            if (this.activeFilter === 'uncompleted') {
+                return this.todos.filter(todo => todo.status !== 'completed');
+            } else if (this.activeFilter === 'completed') {
+                return this.todos.filter(todo => todo.status === 'completed');
+            } else {
+                return this.todos;
+            }
+        },
+        themeDark(){
+            document.body.classList.remove('ligth-theme');
+        },
+        themeLight(){
+            document.body.classList.add('ligth-theme');
         }
-    }
+    },
 });
 
 app.mount('#app');
